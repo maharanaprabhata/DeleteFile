8711781d645c70ed0c899b9df8e1e505fb8419d8
administrator <administrator@DESKTOP-F7RPT76>
2018-04-23 18:34:15 +0530

    hello world program

--- DeleteDllFile/Program.cs
+++ DeleteDllFile/Program.cs
@@ -10,6 +10,8 @@ namespace DeleteDllFile
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!!");
+            Console.ReadLine();
         }
     }
 }
